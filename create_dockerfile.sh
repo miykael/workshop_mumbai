2832c686734a74263a2da065c81cec4f78931989
miykael <michaelnotter@hotmail.com>
2018-07-09 09:55:00 +0200

    WIP: updates path to dataset and datasink file

--- create_dockerfile.sh
+++ create_dockerfile.sh
@@ -20,8 +20,8 @@ docker run --rm kaczmarj/neurodocker:master generate docker \
            --run 'mkdir /templates && chmod 777 /templates && chmod a+s /templates' \
            --run 'rm -rf /opt/conda/pkgs/*' \
            --user=neuro \
-           --run 'curl -L -o /data/dataset.zip https://www.dropbox.com/s/4k4npu7wsz1rtwk/dataset.zip?dl=1 && unzip /data/dataset.zip -d /data/ && rm /data/dataset.zip' \
-           --run 'curl -L -o /output/datasink.zip https://www.dropbox.com/s/torun2z8vchyhsd/datasink.zip?dl=1 && unzip /output/datasink.zip -d /output/ && rm /output/datasink.zip' \
+           --run 'curl -L -o /data/dataset.zip https://www.dropbox.com/sh/fvvium3z3l0wedo/AACkFi2mhWZkcqwmHjO8WwUra?dl=1 && unzip /data/dataset.zip -d /data/ && rm /data/dataset.zip' \
+           --run 'curl -L -o /output/datasink.zip https://www.dropbox.com/sh/dcm0owd4g0028nf/AAAqtZZPusTwbhBrygKXqQNna?dl=1 && unzip /output/datasink.zip -d /output/ && rm /output/datasink.zip' \
            --copy templates "/templates" \
            --copy notebooks "/home/neuro/notebooks" \
            --copy slides "/home/neuro/slides" \
